07fe80d3384a9fe7d4ab80eca2eb8a3b96aa7cc6
Borja <pintocorreas@gmail.com>
2023-09-20 16:44:30 +0200

    Ejercicio 1

--- js/main.js
+++ js/main.js
@@ -5,21 +5,33 @@
 // console.log(raiz)
 
 
-let dia = prompt("Dime un dia de la semana")
+// let dia = prompt("Dime un dia de la semana")
 
-switch(dia){
-    case 'lunes':
-    case 'martes':
-    case 'miercoles':
-    case 'jueves':
-    case 'viernes':
-        console.log("Entre semana")
-    break;
-    case 'sabado':
-    case 'domingo':
-        console.log("fin de  semana")
-    break;
-    default:
-        console.log("Aprendete los dias de la semana")
-    break;
-}
+// switch(dia){
+//     case 'lunes':
+//     case 'martes':
+//     case 'miercoles':
+//     case 'jueves':
+//     case 'viernes':
+//         console.log("Entre semana")
+//     break;
+//     case 'sabado':
+//     case 'domingo':
+//         console.log("fin de  semana")
+//     break;
+//     default:
+//         console.log("Aprendete los dias de la semana")
+//     break;
+// }
+
+//Ejercio 1
+num1 = 2
+num2 = 3
+
+if(num1 === num2){
+    console.log("Son iguales");
+} else if(num1 > num2){
+    console.log(`${num1} es mayor que ${num2}`);
+} else{
+    console.log(`${num1} es menor que ${num2}`);
+};
